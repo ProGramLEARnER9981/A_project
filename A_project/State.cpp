c7f4bb6f3ebb18cfa9f2a180f4eefc5c0193fdaa
SBY <307823745@qq.com>
2021-11-18 10:43:39 +0800

    2.0 use map and set to speed up

--- A_project/State.cpp
+++ A_project/State.cpp
@@ -14,7 +14,7 @@ State::State(table start_table, int d)
 		}
 	}
 	if (zero_col < 0 || zero_row < 0)
-		throw "No zero!";
+		cout<< "No zero!";
 }
 
 State::~State()
@@ -49,8 +49,7 @@ double State::evaluate(unordered_map<int, pos> target_map)
 {
 	if (distance < 0)
 		get_distance(target_map);
-	fvalue = distance + depth*0.5;
-	//cout << depth << " " << distance<<" "<<fvalue << endl;
+	fvalue = distance + depth*1.1;
 	return fvalue;
 }
 
@@ -66,46 +65,48 @@ void State::move_up()
 	data[zero_row][zero_col] = data[zero_row - 1][zero_col];
 	data[zero_row - 1][zero_col] = 0;
 	--zero_row;
+	get_str_num();
 }
+
 void State::move_down()
 {
 	data[zero_row][zero_col] = data[zero_row + 1][zero_col];
 	data[zero_row + 1][zero_col] = 0;
 	++zero_row;
+	get_str_num();
 }
+
 void State::move_left()
 {
 	data[zero_row][zero_col] = data[zero_row][zero_col - 1];
 	data[zero_row][zero_col - 1] = 0;
 	--zero_col;
+	get_str_num();
 }
+
 void State::move_right()
 {
 	data[zero_row][zero_col] = data[zero_row][zero_col + 1];
 	data[zero_row][zero_col + 1] = 0;
 	++zero_col;
+	get_str_num();
 }
 
-
-bool State::equal_to(table t)
+string State::get_str_num()
 {
+	str_num =  table2string(this->data);
+	return str_num;
+}
+
+string table2string(table a)
+{
+	string s;
 	for (int i = 0; i < SIZE; ++i) {
-		for (int j = 0; j < SIZE; ++j) {
-			if (data[i][j] != t[i][j])
-				return false;
-		}
+		for (int j = 0; j < SIZE; ++j)
+			s += to_string(a[i][j]) + "-";
 	}
-	return true;
+	return s;
 }
-
-bool State::equal_to(State* node)
-{
-	if (zero_row != node->zero_row || zero_col != node->zero_col)
-		return false;
-	return node->equal_to(data);
-}
-
-
 
 unordered_map<int, pos> get_target_map(table target)
 {
@@ -119,56 +120,56 @@ unordered_map<int, pos> get_target_map(table target)
 	return target_map;
 }
 
-bool comp(const State* node0, const State* node1)
-{
-	return node0->fvalue < node1->fvalue;
-}
-
-bool node_find(State* node, deque<State*> node_list)
-{
-	deque<State*>::iterator it = node_list.begin();
-	while (it != node_list.end()) {
-		if ((*it)->equal_to(node))
-			return true;
-		++it;
-	}
-	return false;
-}
-
-State* process(State* new_node, deque<State*> &open, deque<State*> &close, const unordered_map<int, pos> target_map)
+State* process(State* new_node, unordered_map<string, State*>& open_map, multiset<State*, cmp>& open_set,
+	unordered_map<string, State*>& close_map, const unordered_map<int, pos> target_map)
 {
 	State* final_node = 0;
-	if (!node_find(new_node, open) && !node_find(new_node, close)) {
+	unordered_map<string, State*>::iterator it_open = open_map.find(new_node->str_num),
+											it_close = close_map.find(new_node->str_num);
+	if (it_open == open_map.end() && it_close == close_map.end()) {
 		new_node->evaluate(target_map);
 		if (new_node->distance == 0) {
 			final_node = new_node;
 		}
-		open.push_back(new_node);
+		open_map.insert(pair<string, State*>(new_node->str_num, new_node));
+		open_set.insert(new_node);
 	}
 	return final_node;
 }
 
-
 deque<State*> A_solution(State* startnode, table target)
 {
-	deque<State*> open, close, solution;
-	State* final_node = 0;
+	State* final_node = 0;   // its data is target {1,2,3...15,0}
+	deque<State*> solution;  // the node chain lead to final_node
+	multiset<State*, cmp> open_set;  // it's sorted by fvalue
 	unordered_map<int, pos> target_map = get_target_map(target);
+	unordered_map<string, State*> open_map, close_map;
 	int node_num = 0;  // the totle num of node <==> the step to the final solution
-	int loop_num = 0;
-	startnode->evaluate(target_map);
-	open.push_back(startnode);
-	while (open.size() && loop_num < 1000000) {
-		sort(open.begin(), open.end(), comp);  // O(n)
-		State* node = open.front();
-		if (loop_num++ % 100 == 0)
-			cout << loop_num << ": " << node->fvalue << " " << node->distance << endl;
-		open.pop_front();
+	int loop_num = 0;  // the loop num of while
 
+	clock_t start1, end1;
+	start1 = clock();
+
+	startnode->evaluate(target_map);
+	open_set.insert(startnode);
+	open_map.insert(pair<string, State*>(startnode->get_str_num(), startnode));
+
+	while (open_set.size() && loop_num < 1000000) {
+
+		State* node = *open_set.begin();
+		open_set.erase(open_set.begin());
+		
+		unordered_map<string, State*>::iterator node_it = open_map.find(node->str_num);
+		open_map.erase(node_it);
+		
+		if (loop_num++ % 500 == 0)
+			cout << loop_num << ": " << node->fvalue << " " 
+				<< node->distance<< " "<< node->depth << endl;
+		
 		if (node->zero_row > 0) {
 			State* new_node = node->extend();
 			new_node->move_up();
-			if (final_node = process(new_node, open, close, target_map))
+			if (final_node = process(new_node, open_map, open_set, close_map, target_map))
 				break;
 			else
 				++node_num;
@@ -176,7 +177,7 @@ deque<State*> A_solution(State* startnode, table target)
 		if (node->zero_row < SIZE - 1) {
 			State* new_node = node->extend();
 			new_node->move_down();
-			if (final_node = process(new_node, open, close, target_map))
+			if (final_node = process(new_node, open_map, open_set, close_map, target_map))
 				break;
 			else
 				++node_num;
@@ -184,7 +185,7 @@ deque<State*> A_solution(State* startnode, table target)
 		if (node->zero_col > 0) {
 			State* new_node = node->extend();
 			new_node->move_left();
-			if (final_node = process(new_node, open, close, target_map))
+			if (final_node = process(new_node, open_map, open_set, close_map, target_map))
 				break;
 			else
 				++node_num;
@@ -192,21 +193,24 @@ deque<State*> A_solution(State* startnode, table target)
 		if (node->zero_col < SIZE - 1) {
 			State* new_node = node->extend();
 			new_node->move_right();
-			if (final_node = process(new_node, open, close, target_map))
+			if (final_node = process(new_node, open_map, open_set, close_map, target_map))
 				break;
 			else
 				++node_num;
 		}
-		close.push_back(node);
+		close_map.insert(pair<string, State*>(node->str_num, node));
 	}
+	end1 = clock();
 	cout << endl << "path: " << endl;
 	while (final_node) {
-		final_node->show_table();
 		solution.push_front(final_node);
 		final_node = final_node->father;
 	}
+	for (auto i = solution.begin(); i != solution.end(); ++i) {
+		(*i)->show_table();
+	}
 
-	std::cout << "node num: " << node_num << ", loop_num: "<< loop_num << endl;
+	std::cout << "node num: " << node_num << ", loop_num: " << loop_num << endl;
+	cout << "total time: " << (double)(end1 - start1) / CLOCKS_PER_SEC << endl;
 	return solution;
 }
-
--- A_project/State.h
+++ A_project/State.h
@@ -1,10 +1,11 @@
 #pragma once
 #include "iostream"  
-#include <set>
 #include <deque>
 #include <stdlib.h>
-#include <algorithm>
+#include <string>
 #include <unordered_map>
+#include <ctime>
+#include <set>
 #define SIZE 4
 using namespace std;
 typedef int table[SIZE][SIZE];   // to store the data
@@ -15,7 +16,6 @@ typedef struct
 	int col;
 }pos;
 
-
 class State
 {
 public:
@@ -24,15 +24,15 @@ public:
 	double fvalue;
 	int depth;  // the depth of state in the tree 
 	double distance;   // the sum of the distance of the num out of position
+	string str_num;    // str of data, used for open/close map
 	State* father;   // previous node
 
 	State(table, int);
 	~State();
-	int get_distance(unordered_map<int, pos>);
-	double evaluate(unordered_map<int, pos>);
-	bool equal_to(table);
-	bool equal_to(State*);
-	void show_table();
+	int get_distance(unordered_map<int, pos>);   // city block distance
+	double evaluate(unordered_map<int, pos>);     
+	string get_str_num();
+	
 
 	State* extend();
 
@@ -40,6 +40,7 @@ public:
 	void move_left();
 	void move_down();
 	void move_right();
+	void show_table();
 
 private:
 	table data;
@@ -47,13 +48,14 @@ private:
 
 
 struct cmp {
-	bool operator()(const State* a, const State* b) {
+	bool operator()(const State* a, const State* b) const
+	{
 		return a->fvalue < b->fvalue;
 	}
 };
 
 unordered_map<int, pos> get_target_map(table target);   // write a table to a map
-bool comp(const State*, const State*);
-bool node_find(State*, deque<State*>);
-deque<State*> A_solution(State*, table);
-State* process(State*, deque<State*>&, deque<State*>&, const unordered_map<int, pos>);
+deque<State*> A_solution(State*, table);    // the main fun
+State* process(State*, unordered_map<string, State*>&, multiset<State*, cmp>&,
+	unordered_map<string, State*>&, const unordered_map<int, pos>);
+string table2string(table);
